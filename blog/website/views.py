65daac2ae93b7d32b124b5f0003a64261bbdf888
Agostinho <agostinho.t.o@gmail.com>
2024-04-12 10:20:32 -0300

    Acesso ao Banco de Dados

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi', 'SystemCtl']
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.all()
+
+    data = {
+        'name': 'Curso de Django 3', 
+        'lista_tecnologias': lista, 
+        'posts': list_posts }
+
+
     return render(request, 'index.html', data)
