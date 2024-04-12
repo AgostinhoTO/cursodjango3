dc54c64aa3a794ced27a4a9c010b2161088fa26b
Agostinho <agostinho.t.o@gmail.com>
2024-04-12 15:31:16 -0300

    Template carregado da internet

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from website.models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'sub_title', 'full_name', 'categories', 'deleted'
+    ]
+    search_fields  = ['title', 'sub_title']
+
+    # ESTA FUNCAO JA EXISTE, VAMOS SOBRESCREVER
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=True)
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
